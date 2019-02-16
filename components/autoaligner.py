8a0003861b2d1ad4a68a5c73239a9928c92aa025
Or <or@gmail.com>
2019-02-17 00:34:53 +0200

    added y axis

--- components/autoaligner.py
+++ components/autoaligner.py
@@ -25,6 +25,17 @@ class AutoAligner:
 
         self.x_enabled = False
 
+
+        self.y_controller = PIDController(
+            1, 0, 0, measurement_source=self.get_y_error)
+
+        self.y_controller.setInputRange(-160, 160)
+        self.y_controller.setOutputRange(-1, 1)
+        self.y_controller.setReference(0)
+        self.y_controller.setPercentTolerance(0.01)
+
+        self.y_enabled = False
+
         self.positioned = 0
 
     def get_x_error(self):
@@ -38,20 +49,30 @@ class AutoAligner:
     def enable_x(self):
         self.x_enabled = True
 
+    def enable_y(self):
+        self.y_enabled = True
+
+    def enable(self):
+        self.enable_x()
+        self.enable_y()
+
     def execute(self):
 
-        if self.x_enabled:
+        if self.x_enabled or self.y_enabled:
             self.drivetrain.lock()
-            output = self.x_controller.update()
-            self.drivetrain.move(output, 0)
+            x_output = self.x_controller.update()
+            y_output = self.y_controller.update()
+            self.drivetrain.move(x_output, y_output)
 
-            if self.x_controller.atReference():
+            if self.x_controller.atReference() and self.y_controller.atReference():
                 self.positioned += 1
             else:
                 self.positioned = 0
 
             if self.positioned == self.CHECKS:
                 self.x_controller.reset()
+                self.y_controller.reset()
                 self.x_enabled = False
+                self.y_enabled = False
                 self.positioned = 0
                 self.drivetrain.unlock()
