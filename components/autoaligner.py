481c0ec646dd0069b6257c795f8ba2b318dfdee4
Nick S <nikeropiukiuk1324@gmail.com>
2019-02-20 02:05:55 +0200

    Added Pathfollower

--- components/autoaligner.py
+++ components/autoaligner.py
@@ -11,78 +11,129 @@ import navx
 
 class AutoAligner:
 
-    CHECKS = 5
-    K = tunable(default=0.2)
+    CHECKS = 10
     Y_SPEED = tunable(default=0.2)
-    MAX_SPEED = tunable(default=0.6)
+    MAX_SPEED_NAVX = tunable(default=0.8)
+    MAX_SPEED_STRIGHT = tunable(default=0.8)
+    ERROR = 5000
 
     drivetrain: Drivetrain
     navx: navx.ahrs.AHRS
 
     def __init__(self):
-
+        self.first_state = self.second_state = self.third_state = self.fourth_state = False
         self.vision = NetworkTables.getTable('Vision')
         self.enabled = False
 
     def setup(self):
 
         self.navx_controller = PIDController(
-            0.015, 0, 0, measurement_source=self.get_navx_error)
+            0.025, 0.01, 0, measurement_source=self.get_navx_error)
 
         self.navx_controller.setInputRange(-180, 180)
-        self.navx_controller.setOutputRange(-self.MAX_SPEED, self.MAX_SPEED)
+        self.navx_controller.setOutputRange(-self.MAX_SPEED_NAVX,
+                                            self.MAX_SPEED_NAVX)
         self.navx_controller.setContinuous()
-        self.navx_controller.setReference(-90)
-        self.navx_controller.setPercentTolerance(0.01)
+        self.navx_controller.setAbsoluteTolerance(3.5)
 
-        self.angle_controller = PIDController(
-            0.1, 0, 0, measurement_source=self.get_angle_error)
+        self.x_controller = PIDController(
+            0.1, 0, 0, measurement_source=self.get_x_error)
 
-        self.angle_controller.setInputRange(-31.1, 31.1)
-        self.angle_controller.setOutputRange(-self.MAX_SPEED, self.MAX_SPEED)
-        self.angle_controller.setReference(0)
-        self.angle_controller.setPercentTolerance(0.01)
+        self.x_controller.setInputRange(-160, 160)
+        self.x_controller.setOutputRange(-self.MAX_SPEED_STRIGHT,
+                                         self.MAX_SPEED_STRIGHT)
+        self.x_controller.setPercentTolerance(0.01)
 
-    def get_angle_error(self):
+        self.reset()
 
-        return -self.vision.getNumber('X Angle', 0)
+    def get_x_error(self):
+
+        return self.vision.getNumber('X Error', 0)
 
     def get_navx_error(self):
 
         return self.navx.getYaw()
 
     def enable(self):
-        self.enabled = True
+        self.state = 1
+        self.navx_controller.setReference(0)
 
     def reset(self):
-        self.angle_controller.reset()
+        self.x_controller.reset()
         self.navx_controller.reset()
-        self.enabled = False
+        self.state = 0
         self.positioned = 0
         self.drivetrain.unlock()
 
     def execute(self):
+        self.vision.putNumber("State", self.state)
 
-        if self.enabled:
+        if self.state == 1:
             self.drivetrain.lock()
-            angle_output = self.angle_controller.update()
-            self.vision.putNumber("AnglePID Output", angle_output)
-
             navx_output = self.navx_controller.update()
             self.vision.putNumber("NavxPID Output", navx_output)
 
             self.drivetrain.move(
-                self.K
-                * angle_output
-                + (self.MAX_SPEED - self.K)
-                * navx_output,
-                self.Y_SPEED
+                navx_output,
+                0
             )
 
-            # if self.x_controller.atReference():
-            #     self.positioned += 1
-            # else:
-            #     self.positioned = 0
+            if self.navx_controller.atReference():
+                self.positioned += 1
+            else:
+                self.positioned = 0
 
-            # if self.positioned == self.CHECKS:
-            #     self.reset()
+            if self.positioned == self.CHECKS:
+                self.state += 1
+                self.x_controller.setReference(0)
+                self.positioned = 0
+
+        if self.state == 2:
+            if self.get_x_error() == self.ERROR:
+                self.drivetrain.move(
+                    0,
+                    self.Y_SPEED
+                )
+            else:
+                x_output = self.x_controller.update()
+                self.vision.putNumber("XPID Output", x_output)
+
+                self.drivetrain.move(
+                    0,
+                    x_output
+                )
+
+                if self.navx_controller.atReference():
+                    self.positioned += 1
+                else:
+                    self.positioned = 0
+
+                if self.positioned == self.CHECKS:
+                    self.state += 1
+                    self.navx_controller.setReference(-90)
+                    self.positioned = 0
+
+        if self.state == 3:
+            navx_output = self.navx_controller.update()
+            self.vision.putNumber("NavxPID Output", navx_output)
+
+            self.drivetrain.move(
+                navx_output,
+                0
+            )
+
+            if self.navx_controller.atReference():
+                self.positioned += 1
+            else:
+                self.positioned = 0
+
+            if self.positioned == self.CHECKS:
+                self.state += 1
+                self.positioned = 0
+
+        if self.state == 4:
+
+            self.drivetrain.move(
+                0,
+                self.Y_SPEED
+            )
